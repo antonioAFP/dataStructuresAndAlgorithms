53a38db1865762de331152ebe47dbb8e4923c1a0
Antonio Flores Pena <radiosabato@hotmail.com>
2019-08-13 18:01:13 -0500

    MajorTriangularMatrix added

--- cpp/matrices/triangularMatrix/main.cpp
+++ cpp/matrices/triangularMatrix/main.cpp
@@ -52,6 +52,58 @@ LowerTriangularMatrix::~LowerTriangularMatrix(){
   delete []array;
 }
 
+
+/*******/
+class MajorTriangularMatrix{
+private:
+  int n;
+  int *array;
+
+public:
+  MajorTriangularMatrix(){
+    this->n = 2;
+    array = new int[2*(2+1)/2];
+  }
+
+  MajorTriangularMatrix(int n){
+    this->n = n;
+    array = new int[n*(n+1)/2];
+  }
+
+  void set(int i, int j, int x);
+  int get(int i, int j);
+  void display();
+  ~MajorTriangularMatrix();
+};
+
+void MajorTriangularMatrix::set(int i, int j, int x){
+  if(i >= j)
+    array[n*(j-1)-(j-2)*(j-1)/2+i-j] = x;
+}
+
+int MajorTriangularMatrix::get(int i, int j){
+  if(i >= j)
+    return array[n*(j-1)-(j-2)*(j-1)/2+i-j];
+  else
+    return 0;
+}
+
+void MajorTriangularMatrix::display(){
+  for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+          if(i >= j)
+            cout << array[n*(j-1)-(j-2)*(j-1)/2+i-j] << " ";
+          else
+            cout << "0 ";
+        }
+      cout << endl;
+    }
+}
+
+MajorTriangularMatrix::~MajorTriangularMatrix(){
+  delete []array;
+}
+
 int main()
 {
   cout << "Hello World!" << endl;
