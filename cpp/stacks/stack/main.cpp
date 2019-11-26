72e684452acc2fa0cf2df5b2f99fb95239791d34
Antonio Flores Pena <radiosabato@hotmail.com>
2019-11-26 17:45:49 -0600

    infix to Postfix() function

--- cpp/stacks/stack/main.cpp
+++ cpp/stacks/stack/main.cpp
@@ -2,6 +2,9 @@
 #include "Stack.h"
 #include <QDebug>
 
+/* Check if a expression is parenthesis balanced,
+    example: ((a+b)*(c+d))  == true
+*/
 bool isBalanced(const char *expression){
   Stack stack;
 
@@ -22,8 +25,51 @@ bool isBalanced(const char *expression){
             return false;
         }
   }
-
   return stack.isEmpty() ? true : false;
+}
+
+bool isOperand(char x){
+  if(x == '+' || x == '-' ||
+     x == '*' || x == '/')
+    return false;
+  else
+    return true;
+}
+
+int precedence(char x){
+   if(x == '+' || x == '-')
+     return 1;
+   else if(x == '*' || x == '/')
+     return 2;
+   else
+     return 0;
+}
+
+char * toPostFix(char *infix){
+  Stack stack;
+  char *postfix = new char[strlen(infix) + 1];
+
+  int i = 0;
+  int j = 0;
+
+  while(infix[i] != NULL){
+      if(isOperand(infix[i]))
+        postfix[j++] = infix[i++];
+      else {
+          if(precedence(infix[i]) > precedence(stack.stackTop()))
+            stack.push(infix[i++]);
+          else
+            postfix[j++] = stack.pop();
+        }
+    }
+
+  while (!stack.isEmpty()) {
+      postfix[j++] = stack.pop();
+    }
+
+  postfix[j] = NULL;
+
+  return  postfix;
 }
 
 int main(int argc, char *argv[])
@@ -48,6 +94,12 @@ int main(int argc, char *argv[])
 
   char *formula2 = "{([a+b))*[c-d]/e}";
   qDebug() << formula2 << " Is balanced: " << isBalanced(formula2);
+
+  char *infix = "a+b*c";
+  qDebug() << infix << " to postfix : " << toPostFix(infix);
+
+  char *infix1 = "a+b*c-d/e";
+  qDebug() << infix1 << " to postfix : " << toPostFix(infix1);
 
   return a.exec();
 }
