8ad008d6c852ad132550aedaf7a80a396f903650
Antonio Flores Pena <radiosabato@hotmail.com>
2019-12-17 17:59:28 -0600

    countSort() and shellSort()

--- cpp/sorting/main.cpp
+++ cpp/sorting/main.cpp
@@ -142,6 +142,60 @@ void recursiveMergeSort(int *a, int low, int high){
     }
 }
 
+int findMax(int *a, int n){
+  int max = INT32_MIN;
+
+  for (int i = 0;i < n;i++) {
+      if(a[i] > max)
+        max = a[i];
+    }
+
+  return max;
+}
+
+void countSort(int *a, int n){
+  int i, j, max, *c;
+
+  max = findMax(a, n);
+
+  c = new int[max + 1];
+
+  for (i = 0; i < n + 1; i++) {
+      c[i] = 0;
+    }
+
+  for (i = 0; i < n; i++) {
+      c[a[i]]++;
+    }
+
+  i = 0;
+  j = 0;
+
+  while(j<max+1){
+      if(c[j] > 0){
+          a[i++] = j;
+          c[j]--;
+        }else
+          j++;
+    }
+}
+
+void shellSort(int *a, int n){
+  int i, j, gap;
+
+  for(gap = n/2; gap > 1; gap/=2){
+      for (i = gap; i < n; i++) {
+          int tmp = a[i];
+          j = i - gap;
+          while (j > 0 && a[j] > tmp) {
+              a[j + gap] = a[j];
+              j = j - gap;
+            }
+          a[j + gap] = tmp;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
@@ -152,6 +206,8 @@ int main(int argc, char *argv[])
   int quickSortArr[] = {3, 7, 9, 10, 6, 5, 12, 4, 2, 11, INT32_MAX};
   int mergeSortArr[] = {3, 7, 9, 10, 6, 5, 12, 4, 2, 11};
   int recMergeSortArr[] = {3, 7, 9, 10, 6, 5, 12, 4, 2, 11};
+  int countSortArr[] = {3, 7, 9, 10, 6, 5, 12, 4, 2, 11};
+  int shellSortArr[] = {3, 7, 9, 10, 6, 5, 12, 4, 2, 11};
   int n = 10;
 
   cout << "Original Array: " ;
@@ -181,6 +237,14 @@ int main(int argc, char *argv[])
   cout << "Rec Merge Sort: " ;
   recursiveMergeSort(recMergeSortArr, 0, n);
   display(recMergeSortArr, n);
+
+  cout << "Count Sort:     " ;
+  countSort(countSortArr, n);
+  display(countSortArr, n);
+
+  cout << "Shell Sort:     " ;
+  shellSort(shellSortArr, n + 1);
+  display(shellSortArr, n);
 
   return a.exec();
 }
